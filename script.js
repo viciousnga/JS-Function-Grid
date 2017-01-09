c31891d9c74a98a427142161b6945736f7eebc48
Ngawang Salaka <ngawangs1180@queensvoc.org>
2017-01-09 22:18:36 +0000

    I made S

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //Write your function calls below this line
+setColor(0, 7, "green");  
+setColor(0, 8, "green"); 
+setColor(0, 9, "green");
+setColor(1, 7, "green");
+setColor(2, 7, "green");
+setColor(2, 8, "green");
+setColor(2, 9, "green");
+setColor(3, 9, "green");
+setColor(4, 9, "green");
+setColor(4, 8, "green");
+setColor(4, 7, "green");
